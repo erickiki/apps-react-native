c6b28312fc9e9dfa53794ee957ddbbc4dd2ebb19
erickiki <ikiquisbert@gmail.com>
2022-12-12 18:18:48 -0400

    primer commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react';
+import Icon  from'react-native-vector-icons/MaterialIcons';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Icon name="person" size={50} />
+    </View>
+  )
+}
+
+export default App
